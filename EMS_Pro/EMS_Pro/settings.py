d61abd70ce0665a5af83c1e27e3d5384de236e27
jaganbaiju <jaganbaiju257@gmail.com>
2026-01-11 18:23:23 +0530

    code updated

--- EMS_Pro/EMS_Pro/settings.py
+++ EMS_Pro/EMS_Pro/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
